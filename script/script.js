96338b7de4176a9a4c209e0d0fd10d7e070bdfba
Iván López Jiménez <113924974+IvanLopezJimenez@users.noreply.github.com>
2025-03-21 13:10:01 +0100

    Actualización 21/03

--- script/script.js
+++ script/script.js
@@ -15,3 +15,32 @@ $(window).scroll(function () {
         }
     });
 });
+
+$(document).ready(function () {
+    // Evita que el menú se cierre automáticamente en desktop
+    $('.navbar-nav .dropdown').hover(
+        function () {
+            $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(200);
+        },
+        function () {
+            $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(200);
+        }
+    );
+
+    // Permite abrir el menú en clic en móviles
+    $('.dropdown-toggle').click(function (e) {
+        if ($(window).width() < 992) {
+            e.preventDefault();
+            $(this).next('.dropdown-menu').slideToggle();
+        }
+    });
+});
+
+window.onload = function() {
+    // Retraso para esperar que cargue la página y permitir autoplay
+    setTimeout(function() {
+        var iframe = document.getElementById('hero-video');
+        var src = iframe.src;
+        iframe.src = src + "&autoplay=1";  // Añadir autoplay después de un retraso
+    }, 1000);  // 1 segundo de retraso
+};
